fcebf85676d8a34d8ef8079edb052ae8f64c2732
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-08-23 00:47:11 -0700

    svc and gridsearch
    given a hyperparameter space, grid search selects evenly spaced points (in linear or log space) from this space, a derivative-free analog to gradient descent.

--- yosi_genesis.py
+++ yosi_genesis.py
@@ -7,9 +7,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-##############################
-# yosi_genesis linear      #
-##############################
 
 def loadRaw(input_file):
 """
@@ -23,3 +20,24 @@ y = data[:, -1]
 return (X, y)
 
 X, y = loadRaw('input3.csv')
+
+# Stratified split test-train data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42) 
+terminator_svm = svm.SVC()
+
+##############################
+# yogi_genesis linear      #
+##############################
+
+#Parameter laden model search 
+parameters = { 'kernel' : ('linear', ), 'C' : [0.1, 0.5, 1, 5, 10, 50, 100] }
+clf = GridSearchCV(terminator_svm, parameters, cv=5 )
+clf.fit(X_train, y_train)
+
+print clf.best_estimator_
+print clf.best_score_
+
+#The chosen model
+svc = clf.best_estimator_
+
+#

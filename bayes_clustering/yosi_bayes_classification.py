96ba467cfbb23a004cfc716f70065e145ad47959
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-08-28 03:03:00 -0700

    Define Prior distributions
    Fetch sys.argv element; query event, trajectory, and optimize drift and covariance.

--- bayes_clustering/yosi_bayes_classification.py
+++ bayes_clustering/yosi_bayes_classification.py
@@ -19,4 +19,36 @@ def gauss(mu, cov, x):
     """
 
 d = len(X)
-den = np.sqrt(np.linalg.det(cov))*(2*np.pi)
+den = np.sqrt(np.linalg.det(cov))*(2*np.pi)**(0.5*d)
+num = np.exp(-0.5 * np.dot(x - mu, np.linalg.solve(cov, np.transpose(x - mu))))
+return num/den
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print 'Usage: python yosi_bayes_classification.py <X_train.csv> <y_train.csv> <X_test.csv>'
+        sys.exit(0)
+
+        #Read Data
+        X_train = load_data(sys.argv[1])
+        y_train = load_data(sys.argv[2], True)
+        X_test = load_data(sys.argv[3])
+
+        #Class priors from data
+        N = len(y_train)
+        pi = dict()
+        for i in y_train:
+            pi[i] = pi.get(i, 0) + 1
+
+            for key in pi.keys():
+                pi[key] = pi[key]/float(N)
+
+        n_classes = len(set(y_train))
+        n_dim = X_train.shape[1]
+        X_test_prob = []
+
+        #optimize this so mu and cov are computed just once
+        
+        for x_0 in X_test:
+            prob = []
+            for y in set(y_train):
+                X_i = X_train[y_train == y, :]
